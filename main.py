1a6b4dd980ef27a8c0054a3c62e674215d1d5827
Mahesh Maan <mahesh.maan@greyb.com>
2022-06-10 13:19:08 +0530

    Expose (a) bag of entities encoder and (b) entity vectorizer via the API

--- main.py
+++ main.py
@@ -12,10 +12,11 @@ from fastapi import FastAPI, Response
 from typing import Union, List, Literal
 from pydantic import BaseModel
 from core.vectorizers import SentBERTVectorizer
+from core.encoders import default_boe_encoder, default_embedding_matrix
 
 class EncodingRequest(BaseModel):
     data: Union[str, List[str]]
-    encoder: Literal['sbert', 'boe']
+    encoder: Literal['sbert', 'boe', 'emb']
 
 app = FastAPI()
 
@@ -27,12 +28,25 @@ async def encode(req: EncodingRequest):
     if req.encoder == "sbert":
         if isinstance(req.data, list):
             vector = SentBERTVectorizer().encode_many(req.data)
-            return {"text": req.data, "vector": vector.tolist()}
+            return {"original": req.data, "encoded": vector.tolist()}
         elif isinstance(req.data, str):
             vectors = SentBERTVectorizer().encode_many(req.data)
-            return {"text": req.data, "vector": vectors.tolist()}
-
+            return {"original": req.data, "encoded": vectors.tolist()}
+    if req.encoder == "boe":
+        if isinstance(req.data, list):
+            entities = [default_boe_encoder.encode(text) for text in req.data]
+            return {"original": req.data, "encoded": entities}
+        elif isinstance(req.data, str):
+            entities = default_boe_encoder.encode(req.data)
+            return {"original": req.data, "encoded": entities}
+    if req.encoder == "emb":
+        if isinstance(req.data, list):
+            embeddings = [default_embedding_matrix[e].tolist() for e in req.data]
+            return {"original": req.data, "encoded": embeddings}
+        elif isinstance(req.data, str):
+            embedding = default_embedding_matrix[req.data].tolist()
+            return {"original": req.data, "encoded": embedding}
 
 if __name__ == "__main__":
     port = int(os.environ["PORT"])
-    uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=port)
--- tests/test_server.py
+++ tests/test_server.py
@@ -16,20 +16,64 @@ API_ENDPOINT = "{}://{}:{}".format(PROTOCOL, HOST, PORT)
 class TestAPI(unittest.TestCase):
     
     def setUp(self):
-        pass
+        self.route = f"{API_ENDPOINT}/encode"
 
-    def test__can_encode_text(self):
+    def test__can_vectorize_text_single(self):
         text = "Some random text"
-        url = "{}/encode".format(API_ENDPOINT)
         payload = {"data": text, "encoder": 'sbert'}
-        response = requests.post(url, json=payload)
+        response = requests.post(self.route, json=payload)
         self.assertEqual(200, response.status_code)
         data = response.json()
-        self.assertIsInstance(data, dict)
-        self.assertIsInstance(data["vector"], list)
+        self.assertEqual(data["original"], text)
+        self.assertIsInstance(data["encoded"], list)
+        self.assertTrue(all(isinstance(x, float) for x in data["encoded"]))
+
+    def test__can_vectorize_text_multiple(self):
+        texts = ["Some random text", "Another random piece of text"]
+        payload = {"data": texts, "encoder": 'sbert'}
+        response = requests.post(self.route, json=payload)
+        self.assertEqual(200, response.status_code)
+        data = response.json()
+        self.assertEqual(data["original"], texts)
+        self.assertIsInstance(data["encoded"], list)
+        self.assertTrue(all(isinstance(x, list) for x in data["encoded"]))
+
+    def test__can_create_bag_of_entities(self):
+        text = "The present invention describes a computer for playing games."
+        payload = {"data": text, "encoder": "boe"}
+        response = requests.post(self.route, json=payload)
+        self.assertEqual(200, response.status_code)
+        data = response.json()
+        self.assertEqual(data["original"], text)
+        self.assertIsInstance(data["encoded"], list)
+        self.assertTrue(all(isinstance(x, str) for x in data["encoded"]))
+
+    def test__can_return_embedding_for_one_entity(self):
+        entity = "computer"
+        payload = {"data": entity, "encoder": "emb"}
+        response = requests.post(self.route, json=payload)
+        self.assertEqual(200, response.status_code)
+        data = response.json()
+        self.assertEqual(data["original"], entity)
+        self.assertIsInstance(data["encoded"], list)
+        self.assertTrue(all(isinstance(x, float) for x in data["encoded"]))
+
+    def test__can_return_embedding_for_multiple_entities(self):
+        entities = ["computer", "games"]
+        payload = {"data": entities, "encoder": "emb"}
+        response = requests.post(self.route, json=payload)
+        self.assertEqual(200, response.status_code)
+        data = response.json()
+        self.assertEqual(data["original"], entities)
+        self.assertIsInstance(data["encoded"], list)
+        self.assertTrue(all(isinstance(x, list) for x in data["encoded"]))
 
     def test__throws_error_when_no_encoder_specified(self):
-        pass
+        text = "Some random text"
+        payload = {"data": text} # no encoder parameter!
+        response = requests.post(self.route, json=payload)
+        self.assertEqual(422, response.status_code)
+    
 
 if __name__ == "__main__":
     unittest.main()
