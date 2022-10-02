2586c9e6b3e78cd2b85ff2a6ab3dce818a08ee56
Mahesh Maan <mahesh.maan@greyb.com>
2022-10-02 23:51:15 +0530

    Obviate the need to have the server running for API testing

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,35 +1,29 @@
 import unittest
-import requests
-import os
-import socket
-from dotenv import load_dotenv
+import sys
 from pathlib import Path
+from dotenv import load_dotenv
+from fastapi.testclient import TestClient
 
-env_file = str((Path(__file__).parent.parent / ".env").resolve())
-load_dotenv(env_file)
+BASE_PATH = Path(__file__).resolve().parent.parent
+ENV_PATH = BASE_PATH / ".env"
 
-PROTOCOL = "http"
-HOST = "localhost"
-PORT = os.environ["PORT"]
-API_ENDPOINT = "{}://{}:{}".format(PROTOCOL, HOST, PORT)
+sys.path.append(BASE_PATH.as_posix())
+load_dotenv(ENV_PATH.as_posix())
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_not_running = sock.connect_ex((HOST, int(PORT))) != 0
-
-if server_not_running:
-    print("Server is not running. API tests will be skipped.")
+from main import app
 
 
-@unittest.skipIf(server_not_running, "Works only when true")
 class TestAPI(unittest.TestCase):
+    
     def setUp(self):
-        self.route = f"{API_ENDPOINT}/encode"
+        self.client = TestClient(app)
+        self.client.testing = True
 
     def test__can_vectorize_text_single(self):
         for encoder in ["sbert", "sif"]:
             text = "Some random text"
             payload = {"data": text, "encoder": encoder}
-            response = requests.post(self.route, json=payload)
+            response = self.client.post("/encode", json=payload)
             self.assertEqual(200, response.status_code)
             data = response.json()
             self.assertEqual(data["original"], text)
@@ -40,7 +34,7 @@ class TestAPI(unittest.TestCase):
         for encoder in ["sbert", "sif"]:
             texts = ["Some random text", "Another random piece of text"]
             payload = {"data": texts, "encoder": encoder}
-            response = requests.post(self.route, json=payload)
+            response = self.client.post("/encode", json=payload)
             self.assertEqual(200, response.status_code)
             data = response.json()
             self.assertEqual(data["original"], texts)
@@ -50,7 +44,7 @@ class TestAPI(unittest.TestCase):
     def test__can_create_bag_of_entities(self):
         text = "The present invention describes a computer for playing games."
         payload = {"data": text, "encoder": "boe"}
-        response = requests.post(self.route, json=payload)
+        response = self.client.post("/encode", json=payload)
         self.assertEqual(200, response.status_code)
         data = response.json()
         self.assertEqual(data["original"], text)
@@ -60,7 +54,7 @@ class TestAPI(unittest.TestCase):
     def test__can_return_embedding_for_one_entity(self):
         entity = "computer"
         payload = {"data": entity, "encoder": "emb"}
-        response = requests.post(self.route, json=payload)
+        response = self.client.post("/encode", json=payload)
         self.assertEqual(200, response.status_code)
         data = response.json()
         self.assertEqual(data["original"], entity)
@@ -70,7 +64,7 @@ class TestAPI(unittest.TestCase):
     def test__can_return_embedding_for_multiple_entities(self):
         entities = ["computer", "games"]
         payload = {"data": entities, "encoder": "emb"}
-        response = requests.post(self.route, json=payload)
+        response = self.client.post("/encode", json=payload)
         self.assertEqual(200, response.status_code)
         data = response.json()
         self.assertEqual(data["original"], entities)
@@ -80,7 +74,7 @@ class TestAPI(unittest.TestCase):
     def test__throws_error_when_no_encoder_specified(self):
         text = "Some random text"
         payload = {"data": text}  # no encoder parameter!
-        response = requests.post(self.route, json=payload)
+        response = self.client.post("/encode", json=payload)
         self.assertEqual(422, response.status_code)
 
 
